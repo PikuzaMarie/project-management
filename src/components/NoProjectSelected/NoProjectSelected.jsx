4d91dd2f535a217d55c65a7fabaa6ff7ff5aba7a
PikuzaMarie <pikuza.mari@gmail.com>
2025-01-24 15:32:47 +0300

    feat: create a component with fallback content
    if no project is selected or add project button is not clicked, display NoProjectSelected component

---
+++ src/components/NoProjectSelected/NoProjectSelected.jsx
@@ -0,0 +1,23 @@
+import { PrimaryButton } from '../PrimaryButton';
+import noProjectImage from '../../assets/no-projects.png';
+
+export function NoProjectSelected() {
+  return (
+    <div className="mt-24 w-2/3 text-center">
+      <img
+        src={noProjectImage}
+        alt="An empty task list with a pen"
+        className="mx-auto h-16 w-16 object-contain"
+      />
+      <h2 className="my-4 text-xl font-bold text-stone-500">
+        No project Selected
+      </h2>
+      <p className="mb-4 text-stone-400">
+        Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <PrimaryButton>Create new project</PrimaryButton>
+      </p>
+    </div>
+  );
+}
