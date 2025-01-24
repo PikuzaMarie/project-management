a115a5a472cce26d5b78ec3490e0aff7c0e976c3
PikuzaMarie <pikuza.mari@gmail.com>
2025-01-24 21:50:40 +0300

    feat: validate required inputs
    show modal if user attempts to save a project without a title or due date

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -1,17 +1,32 @@
 import { useRef } from 'react';
 import { Input } from '../Input';
 import { PrimaryButton } from '../PrimaryButton';
+import { Modal } from '../Modal';
 
 export function NewProject({ onAddProject, onCancelAddProject }) {
+  const modal = useRef();
+
   const titleRef = useRef();
   const descRef = useRef();
   const dateRef = useRef();
 
   const handleCreateProject = () => {
+    const enteredTitle = titleRef.current.value;
+    const enteredDescription = descRef.current.value;
+    const enteredDate = dateRef.current.value;
+
+    if (
+      titleRef.current.value.trim() === '' ||
+      dateRef.current.value.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
     const project = {
-      title: titleRef.current.value,
-      descripton: descRef.current.value,
-      date: dateRef.current.value,
+      title: enteredTitle,
+      descripton: enteredDescription,
+      date: enteredDate,
     };
 
     onAddProject(project);
@@ -30,25 +45,35 @@ export function NewProject({ onAddProject, onCancelAddProject }) {
   };
 
   return (
-    <div className="mt-16 w-[35rem]">
-      <menu className="my-4 flex items-center justify-end gap-4">
-        <li>
-          <button
-            className="curson-pointer text-stone-800 hover:text-stone-950"
-            onClick={handleCancelCreation}
-          >
-            Cancel
-          </button>
-        </li>
-        <li>
-          <PrimaryButton onClick={handleCreateProject}>Save</PrimaryButton>
-        </li>
-      </menu>
-      <div>
-        <Input ref={titleRef} label="Title" type="text" required />
-        <Input ref={descRef} label="Description" type="text" textarea />
-        <Input ref={dateRef} label="Due date" type="date" />
+    <>
+      <Modal ref={modal} buttonCaption="Ok, got it">
+        <h2 className="mb-4 font-bold text-stone-900 uppercase md:text-xl">
+          Invalid input
+        </h2>
+        <p className="mb-6 text-stone-400">
+          Oops.. looks like you forgot to enter a title or due date :))
+        </p>
+      </Modal>
+      <div className="mt-16 w-[35rem]">
+        <menu className="my-4 flex items-center justify-end gap-4">
+          <li>
+            <button
+              className="curson-pointer text-stone-800 hover:text-stone-950"
+              onClick={handleCancelCreation}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <PrimaryButton onClick={handleCreateProject}>Save</PrimaryButton>
+          </li>
+        </menu>
+        <div>
+          <Input ref={titleRef} label="Title *" type="text" required />
+          <Input ref={descRef} label="Description" type="text" textarea />
+          <Input ref={dateRef} label="Due date *" type="date" required />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
