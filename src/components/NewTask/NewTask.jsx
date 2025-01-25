e5ec14ab7ff8b34da56d189a0c50e575cad76212
PikuzaMarie <pikuza.mari@gmail.com>
2025-01-25 15:31:00 +0300

    feat: validate task input
    show modal if user attempts to save an empty task

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,31 +1,48 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { Modal } from '../Modal';
 
 export function NewTask({ onAddTask }) {
   const [taskValue, setTaskValue] = useState('');
+  const modal = useRef();
 
   const handleInputTask = value => {
     setTaskValue(value);
   };
 
   const handleClick = () => {
+    if (!taskValue.trim()) {
+      modal.current.open();
+      return;
+    }
     onAddTask(taskValue);
     setTaskValue('');
   };
 
   return (
-    <div className="flex items-center gap-2">
-      <input
-        type="text"
-        value={taskValue}
-        onChange={e => handleInputTask(e.target.value)}
-        className="w-64 rounded-sm border-b-2 border-stone-200 bg-stone-200 px-2 py-1 focus:border-stone-600 focus:outline-none"
-      />
-      <button
-        className="rounded-sm px-2 py-1 text-stone-600 hover:text-stone-800"
-        onClick={handleClick}
-      >
-        Add a task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Ok, great">
+        <h2 className="mb-4 font-bold text-stone-900 uppercase md:text-xl">
+          Invalid input
+        </h2>
+        <p className="mb-6 text-stone-400">
+          Oops.. you`re trying to add an empty task
+        </p>
+      </Modal>
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          value={taskValue}
+          onChange={e => handleInputTask(e.target.value)}
+          className="w-64 rounded-sm border-b-2 border-stone-200 bg-stone-200 px-2 py-1 focus:border-stone-600 focus:outline-none"
+          required
+        />
+        <button
+          className="rounded-sm px-2 py-1 text-stone-600 hover:text-stone-800"
+          onClick={handleClick}
+        >
+          Add a task
+        </button>
+      </div>
+    </>
   );
 }
